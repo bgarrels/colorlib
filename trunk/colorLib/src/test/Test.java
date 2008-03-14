24282a5504ab6388e35ebe73a549de292acd0367
koeberle <koeberle@eb77233a-4348-0410-91e1-bfab5a1ae557>
2008-03-14 22:44:03 +0000

    Adding a nicer preview drawing for the palettes, fixing a bug in the Kuler query method

--- trunk/colorLib/src/test/Test.java
+++ trunk/colorLib/src/test/Test.java
@@ -14,27 +14,29 @@ public class Test extends PApplet {
 	}
 
 	public void setup() {
-		size(500, 500);
+		size(1400, 490);
 		noStroke();
+		background(100);
 //		p=new Palette(this);
 //		p.makeComplementary(color(6, 131, 189));
-		c = new Colr(this);
-//		k.getRandom();
-//		k.draw();
+//		c = new Colr(this);
+		k = new Kuler(this);
+		k.search("coffee","tag");
+		k.draw();
 	}
 
 	public void draw() {
 		
 	}
 //	
-//	public void keyPressed(){
+	public void keyPressed(){
 //		if(key=='+'){
 //			p.saturate();
 //		}else if(key=='-'){
 //			p.desaturate();
 //		}
 //		println(p.getNearestHues());
-//		saveFrame("german.tif");
-//	}
+		saveFrame("german.tif");
+	}
 }
 
