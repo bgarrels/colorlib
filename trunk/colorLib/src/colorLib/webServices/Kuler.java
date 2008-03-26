e85044ddc3012c8824e07339a4fcd0322c9bd259
koeberle <koeberle@eb77233a-4348-0410-91e1-bfab5a1ae557>
2008-03-26 21:26:46 +0000

    Add startIndex to the kuler querry string

--- trunk/colorLib/src/colorLib/webServices/Kuler.java
+++ trunk/colorLib/src/colorLib/webServices/Kuler.java
@@ -8,17 +8,25 @@ import processing.xml.*;
 
 public class Kuler {
 
-	private PApplet parent;
-	private int maxItems=100;
+	private PApplet p;
+
+	private int maxItems = 100;
+
+	private int startIndex = 0;
+
 	ArrayList themes;
 
 	/**
-	 * The Kuler object is an container to query adobes <a href="kuler.adobe.com">kuler</a> service and get the response at an colorLib palette object.
-	 * Take a look at the <a href="http://labs.adobe.com/wiki/index.php/Kule">API</a> to see the possibilities.
-	 * @param parent typically use "this"
+	 * The Kuler object is an container to query adobes <a
+	 * href="kuler.adobe.com">kuler</a> service and get the response at an
+	 * colorLib palette object. Take a look at the <a
+	 * href="http://labs.adobe.com/wiki/index.php/Kule">API</a> to see the
+	 * possibilities.
+	 * 
+	 * @param parent PApplet, typically use "this"
 	 */
 	public Kuler(PApplet parent) {
-		this.parent = parent;
+		p = parent;
 	}
 
 	/**
@@ -69,13 +77,15 @@ public class Kuler {
 	 * filter... possible filters are themeID, userID, email, tag, hex and title
 	 * 
 	 * searches kuler for a string with a search filter
-	 */
-	/**
-	 * @param searchQuery a querString that is use with a searchFilter
-	 * @param searchFilter one of the followong filters: "themeID", "userID", "email", "tag", "hex" and "title"
+	 * 
+	 * @param searchQuery
+	 *            a querString that is use with a searchFilter
+	 * @param searchFilter
+	 *            one of the followong filters: "themeID", "userID", "email",
+	 *            "tag", "hex" and "title"
 	 */
 	public void search(final String searchQuery, final String searchFilter) {
-		makePalettes("&searchQuery="+searchFilter + ":" + searchQuery);
+		makePalettes("&searchQuery=" + searchFilter + ":" + searchQuery);
 	}
 
 	/**
@@ -85,44 +95,59 @@ public class Kuler {
 	 */
 	public void makePalettes(final String querry) {
 		themes = new ArrayList();
-		String url = "http://kuler.adobe.com/kuler/API/rss/search.cfm?itemsPerPage="+maxItems;
+		String url = "http://kuler.adobe.com/kuler/API/rss/search.cfm?itemsPerPage="
+				+ maxItems + "startIndex=" + startIndex;
 		PApplet.println(querry);
-		XMLElement xml = new XMLElement(parent, url + querry);
-		XMLElement[] themeItems = xml.getChildren("channel/item/kuler:themeItem");
-		
+		XMLElement xml = new XMLElement(p, url + querry);
+		XMLElement[] themeItems = xml
+				.getChildren("channel/item/kuler:themeItem");
+
 		for (int i = 0; i < themeItems.length; i++) {
-			KulerTheme kulerTheme = new KulerTheme(parent);
+			KulerTheme kulerTheme = new KulerTheme(p);
 			XMLElement themeItem = themeItems[i];
-			kulerTheme.setThemeID(themeItem.getChildren("kuler:themeID")[0].getContent());
-			kulerTheme.setThemeTitle(themeItem.getChildren("kuler:themeTitle")[0].getContent());
-			kulerTheme.setAuthorID(themeItem.getChildren("kuler:themeAuthor/kuler:authorID")[0].getContent());
-			kulerTheme.setAuthorLabel(themeItem.getChildren("kuler:themeAuthor/kuler:authorLabel")[0].getContent());
-			kulerTheme.setThemeTags(themeItem.getChildren("kuler:themeTags")[0].getContent());
-			kulerTheme.setThemeRating(themeItem.getChildren("kuler:themeRating")[0].getContent());
-			kulerTheme.setThemeDownloadCount(themeItem.getChildren("kuler:themeDownloadCount")[0].getContent());
-			kulerTheme.setThemeCreatedAt(themeItem.getChildren("kuler:themeCreatedAt")[0].getContent());
-			kulerTheme.setThemeEditedAt(themeItem.getChildren("kuler:themeEditedAt")[0].getContent());
-			
-			XMLElement[] themeSwatches = themeItem.getChildren("kuler:themeSwatches/kuler:swatch/kuler:swatchHexColor");
+			kulerTheme.setThemeID(themeItem.getChildren("kuler:themeID")[0]
+					.getContent());
+			kulerTheme
+					.setThemeTitle(themeItem.getChildren("kuler:themeTitle")[0]
+							.getContent());
+			kulerTheme.setAuthorID(themeItem
+					.getChildren("kuler:themeAuthor/kuler:authorID")[0]
+					.getContent());
+			kulerTheme.setAuthorLabel(themeItem
+					.getChildren("kuler:themeAuthor/kuler:authorLabel")[0]
+					.getContent());
+			kulerTheme.setThemeTags(themeItem.getChildren("kuler:themeTags")[0]
+					.getContent());
+			kulerTheme.setThemeRating(themeItem
+					.getChildren("kuler:themeRating")[0].getContent());
+			kulerTheme.setThemeDownloadCount(themeItem
+					.getChildren("kuler:themeDownloadCount")[0].getContent());
+			kulerTheme.setThemeCreatedAt(themeItem
+					.getChildren("kuler:themeCreatedAt")[0].getContent());
+			kulerTheme.setThemeEditedAt(themeItem
+					.getChildren("kuler:themeEditedAt")[0].getContent());
+
+			XMLElement[] themeSwatches = themeItem
+					.getChildren("kuler:themeSwatches/kuler:swatch/kuler:swatchHexColor");
 			int[] colors = new int[themeSwatches.length];
 			for (int j = 0; j < themeSwatches.length; j++) {
-				colors[j] = PApplet.unhex("FF"+themeSwatches[j].getContent());
+				colors[j] = PApplet.unhex("FF" + themeSwatches[j].getContent());
 				System.out.println(themeSwatches[j].getContent());
 			}
 			kulerTheme.setColors(colors);
-			
+
 			kulerTheme.printSettings();
 			themes.add(kulerTheme);
 		}
 	}
-	
+
 	/**
 	 * @return an array of all kulerthemes
 	 */
 	public KulerTheme[] getThemes() {
-		return  (KulerTheme[]) themes.toArray(new KulerTheme[themes.size()]);
+		return (KulerTheme[]) themes.toArray(new KulerTheme[themes.size()]);
 	}
-	
+
 	// TODO Search for tag in all kulerThemes
 	/**
 	 * @param tag
@@ -131,24 +156,58 @@ public class Kuler {
 	public KulerTheme[] searchForTag(final String tag) {
 		return new KulerTheme[0];
 	}
-	
-	public void draw(){
+
+	public void draw() {
 		Iterator iter = themes.iterator();
-		int cnt=0;
-		int y=0;
+		int cnt = 0;
+		int y = 0;
 		while (iter.hasNext()) {
 			KulerTheme theme = (KulerTheme) iter.next();
-			parent.pushMatrix();
-//			parent.translate(10+(cnt%10)*140, 10+y*48);
-			parent.translate(10+(cnt%5)*70, 10+y*70);
+			p.pushMatrix();
+			// parent.translate(10+(cnt%10)*140, 10+y*48);
+			p.translate(10 + (cnt % 5) * 70, 10 + y * 70);
 			theme.drawWheel();
-			parent.popMatrix();
+			p.popMatrix();
 			cnt++;
-//			if(cnt%10==0)y++;
-			if(cnt%5==0)y++;
+			// if(cnt%10==0)y++;
+			if (cnt % 5 == 0)
+				y++;
 		}
 		System.out.print(cnt);
 	}
+
+	/**
+	 * Get the maximum number of items to display on a page, in the range [1..100]. Default is 100.
+	 * @return int maximum number
+	 */
+	public int getMaxItems() {
+		return maxItems;
+	}
+	/**
+	 * Set the maximum number of items to display on a page, in the range [1..100]. Default is 100.
+	 * @param maxItems maximum number
+	 */
+	public void setMaxItems(int maxItems) {
+		this.maxItems = maxItems;
+	}
+
 	
+	/**
+	 * A 0-based index into the list that specifies the first item to display. 
+	 * Default is 0, which displays the first item in the list.
+	 * @return
+	 */
+	public int getStartIndex() {
+		return startIndex;
+	}
+
+	/**
+	 * A 0-based index into the list that specifies the first item to display. 
+	 * Default is 0, which displays the first item in the list.
+	 * @param startIndex
+	 */
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
 
 }
